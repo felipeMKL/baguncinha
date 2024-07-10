de583f09f97d6d29a898ff27d1ed0efd02e5546e
felipe.gssilva2 <felipe.gssilva2@LIM0681949W10-1>
2024-07-10 16:25:51 -0300

    Adicionar arquivos de projeto.

---
+++ LOGIN/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace LOGIN
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_login = new System.Windows.Forms.Label();
+            this.lb_senha = new System.Windows.Forms.Label();
+            this.txt_login = new System.Windows.Forms.TextBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            this.btn_entrar = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_login
+            // 
+            this.lb_login.AutoSize = true;
+            this.lb_login.BackColor = System.Drawing.Color.Transparent;
+            this.lb_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_login.Location = new System.Drawing.Point(152, 138);
+            this.lb_login.Name = "lb_login";
+            this.lb_login.Size = new System.Drawing.Size(47, 20);
+            this.lb_login.TabIndex = 0;
+            this.lb_login.Text = "login";
+            // 
+            // lb_senha
+            // 
+            this.lb_senha.AutoSize = true;
+            this.lb_senha.BackColor = System.Drawing.Color.Transparent;
+            this.lb_senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_senha.Location = new System.Drawing.Point(141, 222);
+            this.lb_senha.Name = "lb_senha";
+            this.lb_senha.Size = new System.Drawing.Size(58, 20);
+            this.lb_senha.TabIndex = 1;
+            this.lb_senha.Text = "senha";
+            // 
+            // txt_login
+            // 
+            this.txt_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_login.Location = new System.Drawing.Point(205, 138);
+            this.txt_login.Name = "txt_login";
+            this.txt_login.Size = new System.Drawing.Size(100, 26);
+            this.txt_login.TabIndex = 1;
+            this.txt_login.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_senha.Location = new System.Drawing.Point(205, 222);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.PasswordChar = '*';
+            this.txt_senha.Size = new System.Drawing.Size(100, 26);
+            this.txt_senha.TabIndex = 2;
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_cancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_cancelar.Location = new System.Drawing.Point(145, 292);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(65, 28);
+            this.btn_cancelar.TabIndex = 4;
+            this.btn_cancelar.Text = "cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = false;
+            this.btn_cancelar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_entrar
+            // 
+            this.btn_entrar.Location = new System.Drawing.Point(243, 292);
+            this.btn_entrar.Name = "btn_entrar";
+            this.btn_entrar.Size = new System.Drawing.Size(61, 28);
+            this.btn_entrar.TabIndex = 3;
+            this.btn_entrar.Text = "entrar";
+            this.btn_entrar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::LOGIN.Properties.Resources.HAYKIUU;
+            this.pictureBox3.Location = new System.Drawing.Point(155, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(149, 81);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::LOGIN.Properties.Resources.OIP_aaaa;
+            this.pictureBox2.Location = new System.Drawing.Point(92, 200);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::LOGIN.Properties.Resources.png_clipart_arturia_business_logo_musical_instruments_individual_retirement_account_logo_business;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 52);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frm_login
+            // 
+            this.AcceptButton = this.btn_entrar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LOGIN.Properties.Resources.asdasdasdasd;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(437, 418);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_entrar);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.txt_login);
+            this.Controls.Add(this.lb_senha);
+            this.Controls.Add(this.lb_login);
+            this.KeyPreview = true;
+            this.Name = "frm_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela De Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_login;
+        private System.Windows.Forms.Label lb_senha;
+        private System.Windows.Forms.TextBox txt_login;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.Button btn_entrar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
